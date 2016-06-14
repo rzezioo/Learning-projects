00616b3cb83386d70799f501cc24c30a896cb5b9
rzezioo <rzezioo@gmail.com>
2016-06-14 08:35:34 -0400

    first solution

--- PA/2001/sil/sil.cpp
+++ PA/2001/sil/sil.cpp
@@ -2,7 +2,17 @@
 #include <cstdio>
 using namespace std;
 
+int factorial(int n) {
+  if(n <= 1) {
+    return 1;
+  } else {
+    return (n*factorial(n-1)) %10;
+  }
+}
+
 int main() {
-  cout << "Hello World" << endl;
+  int x;
+  cin >> x;
+  cout << factorial(x) << endl;
   return 0;
 }
